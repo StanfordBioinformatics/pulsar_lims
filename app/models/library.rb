5fed4f5df8b5c8539266481179699e855d9a303f
Nathaniel Watson <nathan.watson86@gmail.com>
2016-09-08 20:27:30 -0700

    Removed old methods for adding and removing documents from the library model since I had moved that logic into a concern.

--- app/models/library.rb
+++ app/models/library.rb
@@ -10,39 +10,4 @@ class Library < ActiveRecord::Base
 	validates :sequence_ontology_term_id, :biosample_id, :vendor_id,  presence: true
 	#The fkey antibody_id doesn't need to be required since some libraries, such as ATAC-SEq, don't have an antibody.
 
-  def add_documents(document_ids=[])
-    """ 
-    Function : Adds associations to Documents that are stored in self.documents.
-    Args     : document_ids - array of Document IDs.
-    """
-		if document_ids.blank?
-			return
-		end
-    document_ids.each do |d| 
-      if not d.empty?
-        doc = Document.find(d)
-        if not documents.include? doc 
-          documents << doc 
-        end 
-      end 
-    end 
-  end 
-  
-  def remove_documents(document_ids=[])
-    """ 
-    Function : Removes associations to Documents that are stored in self.documents.
-    Args     : document_ids - array of Document IDs.
-    """
-		if document_ids.blank?
-			return
-		end
-    document_ids.each do |d| 
-      doc = Document.find(d)
-      if documents.include? doc 
-        documents.destroy(doc)
-      end 
-    end 
-  end 
-
-
 end
