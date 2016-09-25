32025dc14a4904906c3cf25864db49fab38dbb9a
Nathaniel Watson <nathan.watson86@gmail.com>
2016-09-24 21:50:31 -0700

    Added new specs. Updated spec/rails_helper.rb to include the Warden helpers.

---
+++ spec/features/signing_in_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "Users can sign in" do
+	let!(:user)  { FactoryGirl.create(:user) }
+	
+	scenario "with valid credentials" do
+		visit "/"
+		click_link "Sign in"
+		fill_in "user_email", with: user.email
+		fill_in "user_password", with: "password"
+		click_button "Log in"
+		expect(page).to have_content("Signed in successfully.")
+		expect(page).to have_content("Signed in as #{user.email}")
+	end
+end
