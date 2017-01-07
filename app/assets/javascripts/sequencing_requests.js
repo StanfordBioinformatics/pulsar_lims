991e6c5b4aa19c3b601c8661b93a88d2e3ec72e8
Nathaniel Watson <nathan.watson86@gmail.com>
2017-01-07 15:43:54 -0800

    Made the adding library button very interactive.

--- app/assets/javascripts/sequencing_requests.js
+++ app/assets/javascripts/sequencing_requests.js
@@ -1,6 +1,15 @@
 $(document).ajaxSuccess(function(event,jqXHR,ajaxProps) {
 		if (event.currentTarget.activeElement.id === "add_library") {
-			$("#libraries").append(jqXHR.responseText);
+			$newContent = $(jqXHR.responseText).hide();
+			$librarySelectLabel = $newContent.children("label").first().text("Library");
+			$librarySelectLabel.append('<span>  <a class="remove_library fa fa-remove"></a></span>');
+			$librarySelectLabel.find(".remove_library").first().click(function(even) {
+				$newContent.fadeOut("slow",function() {
+					$(this).remove();
+				})
+			});
+			$("#libraries").append($newContent);
+			$newContent.fadeIn("slow");
 		}
 	}
 )
