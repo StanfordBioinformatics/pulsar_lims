25389deb361b6419d2c26e5427bb232d464a926e
Nathaniel Watson <nathan.watson86@gmail.com>
2017-01-07 16:43:31 -0800

    Added event delegation to remove library selectors.

--- app/assets/javascripts/sequencing_requests.js
+++ app/assets/javascripts/sequencing_requests.js
@@ -1,15 +1,20 @@
 $(document).ajaxSuccess(function(event,jqXHR,ajaxProps) {
 		if (event.currentTarget.activeElement.id === "add_library") {
-			$newContent = $(jqXHR.responseText).hide();
-			$librarySelectLabel = $newContent.children("label").first().text("Library");
-			$librarySelectLabel.append('<span>  <a class="remove_library fa fa-remove"></a></span>');
-			$librarySelectLabel.find(".remove_library").first().click(function(even) {
-				$newContent.fadeOut("slow",function() {
-					$(this).remove();
+//			newContent = jqXHR.responseText;
+//			console.log(newContent);
+			$libraries = $("#libraries");
+			$libraries.append(jqXHR.responseText);
+			$newContent = $libraries.find(".sequencing_request_libraries").last();
+			$newContentLabel = $newContent.children("label").first()
+			$newContentLabel.text("Library")
+			$newContentLabel.append('<span>  <a class="remove_library fa fa-remove"></a></span>');
+			$libraries.on("click",".remove_library", function(event) {
+				$parentSelector = $(this).closest(".sequencing_request_libraries");
+				$parentSelector.fadeOut("slow",function() {
+					$parentSelector.remove();
 				})
 			});
-			$("#libraries").append($newContent);
-			$newContent.fadeIn("slow");
+			$newContent.hide().fadeIn("slow");
 		}
 	}
 )
