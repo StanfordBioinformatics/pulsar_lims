08b779a187b2c59d828a9173da5abdd563f3ccd3
Nathaniel Watson <nathan.watson86@gmail.com>
2017-04-17 17:58:21 -0700

    Updated plate views

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -3,6 +3,11 @@ class Plate < ActiveRecord::Base
   belongs_to :sequencing_library_prep_kit
   belongs_to :vendor
 
+	validates :vendor, presence: true
+	validates :name, presence: true, uniqueness: true	
+	validates :num_rows, presence: true
+	validates :num_cols, presence: true
+
 	def self.policy_class
 		ApplicationPolicy
 	end
