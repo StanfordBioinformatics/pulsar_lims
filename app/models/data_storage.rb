0bb4abe855126c534e0b08e3008c8d015210d0b6
Nathaniel Watson <nathan.watson86@gmail.com>
2017-10-10 22:57:27 -0700

    Fixed bug with validation

--- app/models/data_storage.rb
+++ app/models/data_storage.rb
@@ -5,7 +5,7 @@ class DataStorage < ActiveRecord::Base
 
 	validates :name, presence: true, uniqueness: true
 	validates_presence_of :bucket, if: "self.data_storage_provider.bucket_storage?"
-	validates_uniqueness_of :bucket, scope: :data_provider_id, allow_blank: true
+	validates_uniqueness_of :bucket, scope: :data_storage_provider_id, allow_blank: true
 	validates_presence_of :project_identifier, if: "self.data_storage_provider.name == DataStorageProvider::DNANEXUS"
 
   def self.policy_class
