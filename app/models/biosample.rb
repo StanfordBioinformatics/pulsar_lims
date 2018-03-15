bc0acd6ff2a8525e3cc21c589e5bca2195709bad
Nathaniel Watson <nathankw@encyclia.local>
2018-03-15 00:47:03 -0700

    1

--- app/models/biosample.rb
+++ app/models/biosample.rb
@@ -19,7 +19,7 @@ class Biosample < ActiveRecord::Base
   has_one :crispr_modification, validate: true, dependent: :nullify
   #Note that specifying "dependent: :restrict_with_exception" when triggered will raise ActiveRecord::DeleteRestrictionError
   has_many :starting_biosample_single_cell_sortings, class_name: "SingleCellSorting", foreign_key: :starting_biosample_id, dependent: :restrict_with_exception #the starting biosample used for sorting. Not required.
-  has_one :sorting_biosample_single_cell_sorting, class_name: "SingleCellSorting", foreign_key: :sorting_biosample_id, dependent: :nullify #the starting biosample used for sorting. Not required.
+  has_one :sorting_biosample_single_cell_sorting, class_name: "SingleCellSorting", foreign_key: :sorting_biosample_id, dependent: :restrict_with_error #the starting biosample used for sorting. Not required.
   belongs_to  :user
   belongs_to  :owner, class_name: "User"
   belongs_to  :biosample_term_name
