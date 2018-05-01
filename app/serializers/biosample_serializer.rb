38c5859ae004cc2e7edc9aed1222b0911ce3d470
Nathaniel Watson <nathankw@encyclia.local>
2018-05-01 00:20:31 -0700

    Updated biosample serializer

--- app/serializers/biosample_serializer.rb
+++ app/serializers/biosample_serializer.rb
@@ -1,6 +1,6 @@
 class BiosampleSerializer < ActiveModel::Serializer
   self.root = false
-  attributes :id, :date_biosample_taken, :description, :lot_identifier, :name, :nih_institutional_certification, :passage_number, :starting_amount, :starting_amount_units, :submitter_comments, :tissue_preservation_method, :tube_label, :vendor_product_identifier, :upstream_identifier, :created_at, :updated_at
+  attributes :id, :date_biosample_taken, :description, :lot_identifier, :name, :nih_institutional_certification, :part_of_biosample_id, :passage_number, :starting_amount, :starting_amount_units, :submitter_comments, :tissue_preservation_method, :tube_label, :vendor_product_identifier, :upstream_identifier, :created_at, :updated_at
 
   has_one :biosample_term_name
   has_one :biosample_type
