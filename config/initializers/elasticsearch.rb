ed7574acdf4561b1d951b4bc77ef73b3afddd80d
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-06-03 00:05:18 -0700

    Added authentication to elasticsearch initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,2 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV["FOUNDELASTICSEARCH_URL"]
+#Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV["FOUNDELASTICSEARCH_URL"]
+Elasticsearch::Model.client = Elasticsearch::Client.new url: "https://#{ENV['EL_USER']}:#{ENV['EL_PASSWORD']}@#{ENV['FOUNDELASTICSEARCH_URL'].split("https://")[1]}"
