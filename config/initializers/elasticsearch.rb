58718b7a679ba5cc97207e4b7411d76863c21bc8
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-06-02 23:34:41 -0700

    Fixed error in elasticsearch initializer.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,1 @@
-Article.__elasticsearch__.client = Elasticsearch::Client.new host: ENV["FOUNDELASTICSEARCH_URL"]
+Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV["FOUNDELASTICSEARCH_URL"]
