f85491f9bb7c151a5f3ea394c5208d2c0ba02efa
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-06-04 00:24:25 -0700

    URL encoded elastisearch un and pw

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,2 +1,2 @@
 #heroku Elasticsearch::Transport::Transport::Errors::Unauthorized
-Elasticsearch::Model.client = Elasticsearch::Client.new url: "https://#{ENV['ES_USER']}:#{ENV['ES_PASSWORD']}@#{ENV['FOUNDELASTICSEARCH_URL'].split('https://')[1]}"
+Elasticsearch::Model.client = Elasticsearch::Client.new url: "https://#{URI::encode(ENV['ES_USER'])}:#{URI::encode(ENV['ES_PASSWORD'])}@#{ENV['FOUNDELASTICSEARCH_URL'].split('https://')[1]}"
