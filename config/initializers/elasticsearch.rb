30ca548d15039d8bc33876cd8e2d7d395b5eb71f
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-06-04 18:11:31 -0700

    Updated elasticsearch initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,2 +1,13 @@
 #heroku Elasticsearch::Transport::Transport::Errors::Unauthorized
-Elasticsearch::Model.client = Elasticsearch::Client.new url: "https://#{URI::encode(ENV['ES_USER'])}:#{URI::encode(ENV['ES_PASSWORD'])}@#{ENV['FOUNDELASTICSEARCH_URL'].split('https://')[1]}"
+
+#Elasticsearch::Model.client = Elasticsearch::Client.new url: "https://#{URI::encode(ENV['ES_USER'])}:#{URI::encode(ENV['ES_PASSWORD'])}@#{ENV['FOUNDELASTICSEARCH_URL'].split('https://')[1]}"
+
+if ENV.include?("PROD")
+  Elasticsearch::Model.client = Elasticsearch::Client.new hosts: [{
+    host: ENV["FOUNDELASTICSEARCH_URL"],
+    user: URI::encode(ENV["ES_USER"]),
+    password: URI::encode(ENV["ES_PASSWORD"])
+  }]
+else
+  Elasticsearch::Model.client = Elasticsearch::Client.new host: "http://localhost:9200"
+end
