5f85777fea8f01697c63c552243b63a613fdf955
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-06-28 22:19:55 -0700

    Fixed syntax error

--- app/serializers/chipseq_experiment_serializer.rb
+++ app/serializers/chipseq_experiment_serializer.rb
@@ -1,6 +1,6 @@
 class ChipseqExperimentSerializer < ActiveModel::Serializer
   self.root = false
-  attributes :id :description, :name, :notes, :submitter_comments, :target_id, :upstream_identifier, :wild_type_input_id, :control_biosample_replicate_ids => [], :experiment_biosample_replicate_ids => [], :upstream_identifier, :created_at, :updated_at
+  attributes :id, :description, :name, :notes, :submitter_comments, :target_id, :upstream_identifier, :wild_type_input_id, :control_biosample_replicate_ids => [], :experiment_biosample_replicate_ids => [], :upstream_identifier, :created_at, :updated_at
 
 
   has_one :user
