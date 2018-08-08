848a19e080f930538e225727a88e3fb2b420da30
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-08-07 22:23:41 -0700

    Removed unneccessary data files

--- app/serializers/target_serializer.rb
+++ app/serializers/target_serializer.rb
@@ -2,5 +2,19 @@ class TargetSerializer < ActiveModel::Serializer
   self.root = false
   embed :ids
  
-  attributes :id, :ensembl, :name, :notes, :refseq, :uniprotkb, :upstream_identifier, :created_at, :updated_at
+  attributes :id, 
+             :ensembl, 
+             :name, 
+             :notes, 
+             :refseq, 
+             :uniprotkb, 
+             :upstream_identifier,
+             :created_at,
+             :updated_at
+
+  has_one  :user
+  has_many :antibodies
+  has_many :crispr_constructs
+  has_many :donor_constructs
+
 end
