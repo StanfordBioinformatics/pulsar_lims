54ebf193c7a22e479de5a893baf31f59b1c5ca2b
Nathan Watson <nathankw@DN0a22f478.SUNet>
2018-08-15 15:50:26 -0700

    fixed typo

--- app/models/chipseq_experiment.rb
+++ app/models/chipseq_experiment.rb
@@ -9,7 +9,7 @@ class ChipseqExperiment < ActiveRecord::Base
   belongs_to :target
   has_and_belongs_to_many :documents
   # The starting_biosample is the one that is the ancestor of all the 'control_replicates' and 'replicates'.
-  belongs_to :starting_biosample, -> {}, class_name: "Biosample"
+  belongs_to :starting_biosample, class_name: "Biosample"
   belongs_to :wild_type_control, class_name: "Biosample"
   has_many :control_replicates, -> {controls}, class_name: "Biosample", dependent: :nullify
   has_many :replicates, -> {experimental}, class_name: "Biosample", dependent: :nullify
