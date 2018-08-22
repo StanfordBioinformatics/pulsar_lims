d933d91ddfa17cf073d51ed56c57cfb0921cbf37
Nathan Watson <nathankw@DN0a22f8cd.SUNet>
2018-08-22 14:14:40 -0700

    Added back library_prototype to ssc serializer

--- app/serializers/single_cell_sorting_serializer.rb
+++ app/serializers/single_cell_sorting_serializer.rb
@@ -14,13 +14,7 @@ class SingleCellSortingSerializer < ActiveModel::Serializer
   has_many :documents
   has_many :plates
   has_many :sequencing_requests
-  #has_one  :library_prototype 
-  #Don't add library_prototype since that is accessed via the sorting_biosample.
-  # Furthermore, it'll cause an error if including it that reads:
-  #     NoMethodError (undefined method `read_attribute_for_serialization' for "":String):.
-  #      app/controllers/api/single_cell_sortings_controller.rb:25:in `show'
-  # That is likely due to the fact that sorting_biosample already includes this library.
-  #has_one  :library_prototype 
+  has_one  :library_prototype 
   has_one  :sorting_biosample
   has_one  :starting_biosample
   has_one  :user
