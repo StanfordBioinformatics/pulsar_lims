8a95c0808ff9ee61c127a3e7f3429816a0f2852e
Nathan Watson <nathankw@DN0a22f29d.SUNet>
2018-10-16 15:52:53 -0700

    Removed requirement of needing to have the concentration specified if the concenteration_unit_id is set (which is handy since the latter has a default setting).

--- app/models/library.rb
+++ app/models/library.rb
@@ -41,7 +41,6 @@ class Library < ActiveRecord::Base
   #validates :vendor_id, presence: true
   validates :biosample_id, presence: true, unless: Proc.new {|lib| lib.prototype?}
   validates :concentration_unit, presence: {message: "must be specified when the quantity is specified."}, if: "concentration.present?"
-  validates :concentration, presence: {message: "must be specified when the units are set."}, if: "concentration_unit.present?"
   validates :sequencing_library_prep_kit_id, presence: true
   validate :validate_concentration_units
 
@@ -160,6 +159,9 @@ class Library < ActiveRecord::Base
 
   def validate_concentration_units
     if self.concentration_unit.present?
+      if self.concentration.blank?
+        self.concentration_unit = nil
+      end
       if self.concentration_unit.unit_type != "concentration"
         self.errors.add(:concentration_unit_id, "must be a concentration type of unit.")
         return false
