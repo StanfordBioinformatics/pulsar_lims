c24432402397f9b861df2e389ee33762c67e606a
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-11-12 22:36:14 -0800

    Added paging to search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,10 +7,10 @@ class SearchController < ApplicationController
     query = params[:query]
     # Careful not use use 'controller_name' as a variable name as that is the name of a Rails method.
     if flash[:ctl_name].present?
-      # Need to set the flash since after doing a search, the user will be within the search 
-      # controller realm since a render instead of a redirect is done to get to the model-in-question's 
-      # index view. Because the user can perform another search within the search controller's search 
-      # view, we'll need to cache what model they used. 
+      # Need to set the flash since after doing a search, the user will be within the search
+      # controller realm since a render instead of a redirect is done to get to the model-in-question's
+      # index view. Because the user can perform another search within the search controller's search
+      # view, we'll need to cache what model they used.
       ctl_name = flash[:ctl_name]
     else
       ctl_name = params[:controller_name]
@@ -23,26 +23,26 @@ class SearchController < ApplicationController
     begin
       @model_class = ctl_name.classify.constantize
     rescue NameError
-      # The user was on a controller page that isn't associated with a model, i.e. 
-      # the welcome controller. 
+      # The user was on a controller page that isn't associated with a model, i.e.
+      # the welcome controller.
       @model_class = nil
     end
     # If there isn't a model class for the controller in question, then we'll get a NameError error.
-    # For example, doing a search from a Welcome controller view will result in a NameError with the 
+    # For example, doing a search from a Welcome controller view will result in a NameError with the
     # message "uninitialized constant Welcome". In such a case, we'll search across all Elastic indices
-    # (all models). 
+    # (all models).
     if @model_class.nil?
       # Do a global search
-      @records = Elasticsearch::Model.search(query, models=[], options={size: limit}).records
+      @records = Elasticsearch::Model.search(query, models=[], options={size: limit}).records.page(1)
       render layout: false
       return
     elsif @model_class < ActiveRecord::Base
-      @records = @model_class.search(query, size: limit).records
+      @records = @model_class.search(query, size: limit).records.page(1)
     else
       # Shouldn't get here
       @records = []
       flash[:notice] = "Search failed; contact administrator."
     end
     render "#{ctl_name}/index", layout: false
-  end 
+  end
 end
