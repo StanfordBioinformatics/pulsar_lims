a2323943916ede7b1194cb60b41e7ea7b3e3e540
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-11-29 23:14:45 -0800

    Renamed environment variable ES_PASSWORD to ES_PW.

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,13 +1,13 @@
 #heroku Elasticsearch::Transport::Transport::Errors::Unauthorized
 
-#Elasticsearch::Model.client = Elasticsearch::Client.new url: "https://#{URI::encode(ENV['ES_USER'])}:#{URI::encode(ENV['ES_PASSWORD'])}@#{ENV['FOUNDELASTICSEARCH_URL'].split('https://')[1]}"
+#Elasticsearch::Model.client = Elasticsearch::Client.new url: "https://#{URI::encode(ENV['ES_USER'])}:#{URI::encode(ENV['ES_PW'])}@#{ENV['FOUNDELASTICSEARCH_URL'].split('https://')[1]}"
 
 if ENV.include?("PROD")
   Elasticsearch::Model.client = Elasticsearch::Client.new hosts: [{
     host: ENV["FOUNDELASTICSEARCH_URL"].split("https://")[1],
     user: ENV["ES_USER"],
     port: "9243",
-    password: ENV["ES_PASSWORD"],
+    password: ENV["ES_PW"],
     scheme: "https"
   }]
 else
