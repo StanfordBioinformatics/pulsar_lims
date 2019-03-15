6b521e55c970785bbfe498b591e31cbcf1786077
Nathan Watson <nathankw@DN0a22f208.SUNet>
2019-03-15 15:16:55 -0700

    Removed requirement for pcr to have name.

--- app/models/pcr.rb
+++ app/models/pcr.rb
@@ -12,7 +12,6 @@ class Pcr < ActiveRecord::Base
   belongs_to :pcr_master_mix
   belongs_to :crispr_modification
 
-  validates :name, presence: true
   validates :pcr_master_mix, presence: true
 
 
