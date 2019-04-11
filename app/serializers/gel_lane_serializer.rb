8a441a38260de6bf84924d3772a8ef237399d7f7
Nathan Watson <nathankw@orzo.stanford.edu>
2019-04-11 16:43:26 -0700

    Added serializer property for gel_lane.

--- app/serializers/gel_lane_serializer.rb
+++ app/serializers/gel_lane_serializer.rb
@@ -5,6 +5,7 @@ class GelLaneSerializer < ActiveModel::Serializer
   attributes :id, 
              :expected_product_size, 
              :lane_number, 
+             :low_target_band_intensity,
              :sample_concentration,
              :sample_volume,
              :notes,
