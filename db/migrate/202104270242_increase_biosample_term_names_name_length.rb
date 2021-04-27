76e1305ff46c502bf86d818634788a81c289a6d4
tao <twang15@ncsu.edu>
2021-04-27 14:59:31 -0700

    Issue 252: enlarge the length for bio-term name

--- db/migrate/202104270242_increase_biosample_term_names_name_length.rb
+++ db/migrate/202104270242_increase_biosample_term_names_name_length.rb
@@ -1,4 +1,4 @@
-class IncreaseLengthOfNameForBiosampleTermNames < ActiveRecord::Migration
+class IncreaseBiosampleTermNamesNameLength < ActiveRecord::Migration
   def up
     change_column :biosample_term_names, :name, :string, :limit => 100
   end
